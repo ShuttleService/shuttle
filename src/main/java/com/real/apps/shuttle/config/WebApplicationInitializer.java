9df1caaffa02a0c218ec337c8a35077a9378559a
Zorodzayi Mukuya <mukuya@gmail.com>
2014-11-18 19:04:42 +0200

    Renaming Bootstrap and SecurityBootStrap to WebApplicationInitializer and SecurityWebApplication Initializer to kinda match what they do.

---
+++ src/main/java/com/real/apps/shuttle/config/WebApplicationInitializer.java
@@ -0,0 +1,25 @@
+package com.real.apps.shuttle.config;
+
+import com.real.apps.shuttle.security.SecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Created by zorodzayi on 14/10/04.
+ */
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{MvcConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/com/real/apps/shuttle/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.real.apps.shuttle.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by zorodzayi on 14/11/11.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
