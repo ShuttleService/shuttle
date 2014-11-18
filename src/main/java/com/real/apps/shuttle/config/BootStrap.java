26adb32653490e7a2a01baa4bd33f3f8fb5002f5
Zorodzayi Mukuya <mukuya@gmail.com>
2014-11-18 18:44:34 +0200

    Upping Spring Version To 4.1.1

--- src/main/java/com/real/apps/shuttle/config/BootStrap.java
+++ src/main/java/com/real/apps/shuttle/config/BootStrap.java
@@ -1,5 +1,6 @@
 package com.real.apps.shuttle.config;
 
+import com.real.apps.shuttle.security.SecurityConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
@@ -9,7 +10,7 @@ public class BootStrap extends AbstractAnnotationConfigDispatcherServletInitiali
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{};
+        return new Class<?>[]{SecurityConfig.class};
     }
 
     @Override
