14c7c44ad7738127f4f98350d8d4d75ca87f7387
Zorodzayi Mukuya <mukuya@gmail.com>
2015-01-06 13:19:33 +0200

    Testing Form Login

--- src/test/java/com/real/apps/shuttle/security/SecurityConfigTest.java
+++ src/test/java/com/real/apps/shuttle/security/SecurityConfigTest.java
@@ -1,10 +1,15 @@
 package com.real.apps.shuttle.security;
 
 import com.real.apps.shuttle.config.MvcConfiguration;
+import com.real.apps.shuttle.model.User;
+import com.real.apps.shuttle.repository.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,9 +17,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.Filter;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
 
 /**
  * Created by zorodzayi on 15/01/05.
@@ -28,6 +36,12 @@ public class SecurityConfigTest {
     @Autowired
     private Filter springSecurityFilterChain;
     private MockMvc mockMvc;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private MongoOperations mongoTemplate;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Before
     public void init(){
@@ -37,7 +51,20 @@ public class SecurityConfigTest {
     }
 
     @Test
-    public void shouldSuccessfullyLogInAUserWithValidCredentials(){
-        formLogin().user("user").password("password");
+    public void shouldSuccessfullyLogInAUserWithValidCredentials() throws Exception {
+        String username = "Test User Name To Be Used For Login";
+        String password = "Test Password To Be Used For Login";
+        String role = "ROLE_WORLD";
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setAuthorities(Arrays.asList(new SimpleGrantedAuthority(role)));
+        repository.save(user);
+        mockMvc.perform(formLogin().user(username).password(password)).andExpect(authenticated().withRoles("WORLD"));
+        mongoTemplate.dropCollection("user");
+    }
+    @Test
+    public void shouldNotLogInAUserWithInvalidCredentials() throws Exception {
+        mockMvc.perform(formLogin().user("user").password("wrongPassword")).andExpect(unauthenticated());
     }
 }
